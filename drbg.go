c89064acf690fce45aabffd1d904a52ddc66158b
Chris Coulson <chris.coulson@canonical.com>
2021-02-11 13:29:13 +0000

    Add license information

--- drbg.go
+++ drbg.go
@@ -1,3 +1,7 @@
+// Copyright 2021 Canonical Ltd.
+// Licensed under the LGPLv3 with static-linking exception.
+// See LICENCE file for details.
+
 /*
 Package drbg implements several DRBGs as recommended by NIST SP-800-90A.
 */
--- testdata/gentest.go
+++ testdata/gentest.go
@@ -1,5 +1,4 @@
 // Copyright 2021 Canonical Ltd.
-
 // Licensed under the LGPLv3 with static-linking exception.
 // See LICENCE file for details.
 
